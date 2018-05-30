a4d70a4622c56e996d2e4a08ef780d220c68b2c6
lixinsheng <lixinsheng@levelup.ai>
2018-05-30 16:30:24 +0800

    莫烦教程的final版本

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # 导入环境和学习方法
 from env import ArmEnv
 from rl import DDPG
+import time
 
 # 设置全局变量
 MAX_EPISODES = 500
 MAX_EP_STEPS = 200
-
+ON_TRAIN = False
 # 设置环境
 env = ArmEnv()
 s_dim = env.state_dim
@@ -14,19 +15,46 @@ a_bound = env.action_bound
 
 # 设置学习方法 (这里使用 DDPG)
 rl = DDPG(a_dim, s_dim, a_bound)
-
 # 开始训练
-for i in range(MAX_EPISODES):
-    s = env.reset()                 # 初始化回合设置
-    for j in range(MAX_EP_STEPS):
-        env.render()                # 环境的渲染
-        a = rl.choose_action(s)     # RL 选择动作
-        s_, r, done = env.step(a)   # 在环境中施加动作
 
-        # DDPG 这种强化学习需要存放记忆库
-        rl.store_transition(s, a, r, s_)
+steps = []
 
-        if rl.memory_full:
-            rl.learn()              # 记忆库满了, 开始学习
 
-        s = s_                      # 变为下一回合
+def train():
+    # start training
+    for i in range(MAX_EPISODES):
+        s = env.reset()
+        ep_r = 0.
+        for j in range(MAX_EP_STEPS):
+            env.render()
+            # time.sleep(0.001)
+            a = rl.choose_action(s)
+            s_, r, done = env.step(a)
+            rl.store_transition(s, a, r, s_)
+            ep_r += r
+            if rl.memory_full:
+                # start to learn once has fulfilled the memory
+                rl.learn()
+
+            s = s_
+            if done or j == MAX_EP_STEPS-1:
+                print('Ep: %i | %s | ep_r: %.1f | steps: %i' % (i, '---' if not done else 'done', ep_r, j))
+                break
+    rl.save()
+
+
+def eval():
+    rl.restore()
+    env.render()
+    env.viewer.set_vsync(True)
+    s = env.reset()
+    while True:
+        env.render()
+        a = rl.choose_action(s)
+        s, r, done = env.step(a)
+
+
+if ON_TRAIN:
+    train()
+else:
+    eval()
